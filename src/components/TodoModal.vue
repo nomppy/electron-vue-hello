1123e694b556dcf7f1261510127ff334313d176f
spesthecat <kenhtsun@gmail.com>
2020-06-20 21:17:06 -0400

    modal placeholder - animation tmrw

--- src/components/TodoModal.vue
+++ src/components/TodoModal.vue
@@ -1,6 +1,9 @@
 <template>
-    <div :class="{ 'modal-background': show, 'modal-hide': true }" @click.prevent="$store.commit('todoModal/hideModal')">
-        Hi
+    <div id='todo-modal'>
+        <div :class="{ 'modal-show': show, 'modal': true}">
+            <div :class="{ 'hi-show': show, 'hi': true} "/>
+        </div>
+        <div :class="{ 'modal-background-show': show, 'modal-background': true }" @click.prevent="$store.commit('todoModal/hideModal')"/>
     </div>
 </template>
 
@@ -26,25 +29,47 @@ export default {
 </script>
 
 <style scoped>
-.modal-hide {
-    color: white;
+.hi {
+    transition: transform 0.1s ease;
+    transform: scale(0, 0);
+    height: 100px;
+    width: 100px;
+    background-color: green;
+    z-index: 2;
+}
 
+.hi-show {
+    transform: scale(1, 1);
+}
+
+#todo-modal {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+}
+
+.modal-background {
+    color: white;
     pointer-events: none;
     position: absolute;
     transition: opacity 0.1s ease;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     height: 100%;
     width: 100%;
     background-color: #000;
     opacity: 0;
-    z-index: 2;
+    z-index: 1;
 }
 
-.modal-background {
+.modal-background-show {
     pointer-events: auto;
     opacity: 0.85;
 }
 
+.modal {
+    z-index: 2;
+    pointer-events: all;
+    margin: auto;
+}
 </style>
