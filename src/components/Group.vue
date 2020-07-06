64fb9eb2b0b7eb03f90dbb31c4965dd36afcd0ca
spesthecat <kenhtsun@gmail.com>
2020-07-05 22:46:49 -0400

    still no work rawf

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -3,15 +3,13 @@
         <div id='category'>
             <h3> {{ group.name }} </h3>
         </div>
-        <div id='items'>
-            <ul class='list'>
-                <li class='list-item' v-for="item in getTodosInGroup(group.id)" :key="item.id">
-                    <!-- <Checkbox @click.native="$store.commit('todo/toggleComplete', item.id)" :completed="item.completed"/> -->
-                    <v-checkbox class='checkbox indigo' :color='colors.indigo' :label='item.brief'/>
-                    <!-- <GroupBrief :item="item"/> -->
-                </li>
-            </ul>
-        </div>
+        <ul class='list'>
+            <li class='list-item' v-for="item in getTodosInGroup(group.id)" :key="item.id">
+                <!-- <Checkbox @click.native="$store.commit('todo/toggleComplete', item.id)" :completed="item.completed"/> -->
+                <v-checkbox class='checkbox' color='indigo' label='item.brief'/>
+                <!-- <GroupBrief :item="item"/> -->
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -19,7 +17,6 @@
 // import Checkbox from '@/components/Checkbox.vue';
 // import GroupBrief from '@/components/GroupBrief.vue';
 import { mapGetters, mapMutations } from 'vuex';                                    
-import colors from 'vuetify/lib/util/colors'
 
 
 export default {
@@ -44,6 +41,7 @@ export default {
 
 <style scoped>
 .checkbox {
+
 }
 
 #group {
@@ -57,12 +55,6 @@ export default {
     text-align: center;
     font-size: 20px;
     margin: 10px;
-}
-
-#items {
-    padding: 10px;
-    border-radius: 6px;
-    border: 2px solid rgba(67, 94, 233, 0.733);
 }
 
 .list {
